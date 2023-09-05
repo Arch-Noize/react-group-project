d59ded92df82db6d369ffd8ed5c8c2f7004081b2
dvcorreia1984 <victor@onsleer.co.za>
2023-09-05 13:18:07 +0200

    Updated Missions.js file

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -14,8 +14,12 @@ const Missions = () => {
     <h3>
       {missions.map((mission) => (
         <li key={mission.id}>
-          <h4>{mission.name}</h4>
+          <h3>Mission</h3>
+          <h2>{mission.name}</h2>
+          <h3>Description</h3>
           <p>{mission.description}</p>
+          <p>Mission status</p>
+          <p>Join status</p>
         </li>
       ))}
     </h3>
