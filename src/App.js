944970a21a513ed7f09fea0d449bb9f490fb1420
dvcorreia1984 <victor@onsleer.co.za>
2023-09-07 15:26:35 +0200

    Updated implementation of Dispatch function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import RocketsPage from './pages/RocketsPage';
 import DragonsPage from './components/Dragons';
 import MissionsPage from './pages/MissionsPage';
 import ProfilePage from './pages/ProfilePage';
+import { getMissions } from './redux/mission/missionSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getMissions());
+  }, []);
+
   return (
     <>
       <Navbar />
--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,18 +1,13 @@
 // Missions.js
 
-import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Badge, Button, Table } from 'react-bootstrap';
-import { getMissions, joinMission, leaveMission } from '../redux/mission/missionSlice'; // You need to import these action functions.
+import { joinMission, leaveMission } from '../redux/mission/missionSlice'; // You need to import these action functions.
 import '../styles/Missions.module.css';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missions.missions);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(getMissions());
-  }, [dispatch]);
 
   return (
     <div className="table">
