91aa984ed9c971f8b2700df9d492f3e009107c90
dvcorreia1984 <victor@onsleer.co.za>
2023-09-07 16:42:15 +0200

    Updated Profile section

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Profile = () => {
+  const { missions } = useSelector((state) => state.missions);
   const { dragons } = useSelector((state) => state.dragons);
   const resDragons = dragons.filter((dragon) => dragon.reserved);
+  const resMissions = missions.filter((mission) => mission.reserved);
 
   return (
     <div className="profile">
@@ -12,6 +14,14 @@ const Profile = () => {
         {resDragons.map((dragon) => (
           <li key={dragon.id}>
             {dragon.name}
+          </li>
+        ))}
+      </ul>
+      <h2>Reserved Missions</h2>
+      <ul>
+        {resMissions.map((mission) => (
+          <li key={mission.id}>
+            {mission.name}
           </li>
         ))}
       </ul>
